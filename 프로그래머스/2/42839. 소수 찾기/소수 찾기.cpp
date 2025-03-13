8b54048518b91cd1d11d43ffd4932eb33139455d
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-13 17:37:48 +0900

    [level 2] Title: 소수 찾기, Time: 0.63 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -3,49 +3,46 @@
 #include <algorithm>
 #include <cmath>
 using namespace std;
+
 bool isPrime(int n)
 {
     if(n < 2) return false;
-    
     for(int i = 2; i <= sqrt(n); i++)
     {
-        if(n% i == 0)
-        {
-            return false;
-        }
+        if(n % i == 0) return false;
     }
     
     return true;
 }
 int solution(string numbers) {
     int answer = 0;
+    vector<char> v;
     vector<int> nums;
-    vector <char> v;
-    
-    for(int i = 0; i < numbers.size(); i++)
+    for(char c : numbers)
     {
-        v.push_back(numbers[i]);
+        v.push_back(c);
     }
     sort(v.begin(), v.end());
-    
-    do{
+    do
+    {
         string temp = "";
-        for(int i = 0; i < v.size(); i++)
+        for (int i = 0; i < v.size(); i++) 
         {
             temp.push_back(v[i]);
             nums.push_back(stoi(temp));
         }
-    }while(next_permutation(v.begin(), v.end()));
-    sort(nums.begin(), nums.end());
-    nums.erase(unique(nums.begin(),nums.end()), nums.end());
-    
-    for(int i = 0; i < nums.size(); i++)
-        {
-            if(isPrime(nums[i]))
-            {
-                answer++;
-            }
-        }
+    } while (next_permutation(v.begin(), v.end()));
 
+    sort(nums.begin(), nums.end());
+    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+
+    for(int i = 0; i < nums.size(); i++)
+    {
+        if(isPrime(nums[i]))
+        {
+            answer++;
+        }
+    }
+    
     return answer;
 }
