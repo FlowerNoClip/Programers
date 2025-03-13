4d9cdd6c70b9c9485207b9925931dd3cfd00451f
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-13 19:03:15 +0900

    [level 2] Title: 모음 사전, Time: 0.11 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,32 +1,31 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
-
-vector<char> vowels = {'A', 'E', 'I', 'O', 'U'};
-int cnt = 0;  // 전역 변수를 사용하여 cnt 값을 유지
-
-void generate_combinations(string current_combination, const string& word, int& result) {
-    if (current_combination == word) {
-        result = cnt;
+vector<char> aeiou = {'A','E','I','O','U'};
+int result = 0;
+bool found = false;
+void combination(string answer, int length, const string& word) {
+    if (found) {
         return;
     }
-
-    if (current_combination.size() > 5) {
+    
+    result++;
+    if (answer == word) {
+        found = true;
         return;
     }
-
-    cnt++;  // 모든 조합을 시도할 때마다 증가
-
-    for (char vowel : vowels) {
-        generate_combinations(current_combination + vowel, word, result);
+    if (length == 5) {
+        return;
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        combination(answer + aeiou[i], length + 1, word);
     }
 }
-
 int solution(string word) {
-    int result = 0;
-    cnt = 0;  // cnt를 초기화
-    generate_combinations("", word, result);
-    return result;  // count는 0부터 시작하므로 결과에 1을 더함
+    int answer = 0;
+    combination("", 0, word);
+    
+    return result - 1;
 }
