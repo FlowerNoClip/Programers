6df22a567e250511254ffcd73e2a21156bb96479
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-13 17:41:04 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -6,16 +6,16 @@ using namespace std;
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     int num = brown + yellow;
+    
     for(int i = 3; i <= num/3; i++)
     {
         if(num % i == 0)
         {
             int j = num / i;
-            if((i - 2) * (j - 2) == yellow)
+            if((i-2) * (j-2) == yellow)
             {
                 answer.push_back(j);
                 answer.push_back(i);
-                
                 return answer;
             }
         }
