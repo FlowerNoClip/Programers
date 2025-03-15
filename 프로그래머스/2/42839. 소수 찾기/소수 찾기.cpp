1c39645ca32ed940d81397417ba9360db8953982
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-15 13:27:49 +0900

    [level 2] Title: 소수 찾기, Time: 0.63 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+
 using namespace std;
 
 bool isPrime(int n)
 {
     if(n < 2) return false;
+    
     for(int i = 2; i <= sqrt(n); i++)
     {
         if(n % i == 0) return false;
@@ -23,19 +25,17 @@ int solution(string numbers) {
         v.push_back(c);
     }
     sort(v.begin(), v.end());
-    do
-    {
+    do{
         string temp = "";
         for (int i = 0; i < v.size(); i++) 
         {
             temp.push_back(v[i]);
             nums.push_back(stoi(temp));
         }
-    } while (next_permutation(v.begin(), v.end()));
-
+    }while(next_permutation(v.begin(), v.end()));
     sort(nums.begin(), nums.end());
     nums.erase(unique(nums.begin(), nums.end()), nums.end());
-
+    
     for(int i = 0; i < nums.size(); i++)
     {
         if(isPrime(nums[i]))
