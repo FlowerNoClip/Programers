a368e164ed3a64fe9a8fa48beefa0d2fa72c4c66
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-15 13:16:25 +0900

    [level 1] Title: 모의고사, Time: 0.08 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -6,20 +6,24 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    int one_answers[5] = {1,2,3,4,5};
-    int two_answers[8] = {2,1,2,3,2,4,2,5};
-    int three_answers[10] = {3,3,1,1,2,2,4,4,5,5};
-    int one = 0, two = 0 , three = 0;
+    int one_answer[5] = {1,2,3,4,5};
+    int two_answer[8] = {2,1,2,3,2,4,2,5};
+    int three_answer[10] = {3,3,1,1,2,2,4,4,5,5};
+    
+    int one = 0, two = 0, three = 0;
+    
     for(int i = 0; i < answers.size(); i++)
     {
-        if(answers[i] == one_answers[i%5]) one++;
-        if(answers[i] == two_answers[i%8]) two++;
-        if(answers[i] == three_answers[i%10]) three++;
+        if(answers[i] == one_answer[i%5]) one ++;
+        if(answers[i] == two_answer[i%8]) two ++;
+        if(answers[i] == three_answer[i%10]) three++;
     }
     
-    int max_length = max({one, two, three});
-    if(max_length == one) answer.push_back(1);
-    if(max_length == two) answer.push_back(2);
-    if(max_length == three) answer.push_back(3);
+    int max_ansnum = max({one, two, three});
+    
+    if(max_ansnum == one) answer.push_back(1);
+    if(max_ansnum == two) answer.push_back(2);
+    if(max_ansnum == three) answer.push_back(3);
+    
     return answer;
 }
