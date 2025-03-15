6a41e7bafcaafb34d40e15e091baa6d93ce461dd
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-15 13:11:37 +0900

    [level 1] Title: 최소직사각형, Time: 0.74 ms, Memory: 4.59 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -7,13 +7,14 @@ int solution(vector<vector<int>> sizes) {
     int answer = 0;
     int max_width = 0;
     int max_height = 0;
+    
     for(auto& size : sizes)
     {
         int width = min(size[0], size[1]);
         int height = max(size[0], size[1]);
         
-        max_width = max(width, max_width);
+        max_width = max(width,max_width);
         max_height = max(height, max_height);
     }
-    return max_height * max_width;
+    return max_width * max_height;
 }
