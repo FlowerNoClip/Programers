480e9bda3a0e5decdb42d816e03264fbe171f59a
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-03-17 19:55:34 +0900

    [level 1] Title: 폰켓몬, Time: 0.96 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,22 +1,17 @@
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;
-    int num = nums.size()/2;
-    sort(nums.begin(), nums.end());
-    nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    unordered_set<int> set;
+    for(auto& num : nums)
+    {
+        set.insert(num);
+    }
     
-    if(num > nums.size())
-    {
-        answer = nums.size();
-    }
-    else
-    {
-        answer = num;
-    }
-    return answer;
+    
+    return min(nums.size() / 2 , set.size());
 }
