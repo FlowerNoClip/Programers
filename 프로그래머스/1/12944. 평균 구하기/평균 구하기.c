1b3dac7bfa06952b3ef9480a21f789f1a44dbe8e
SonJi <69899942+FlowerNoClip@users.noreply.github.com>
2025-05-02 13:05:29 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.c
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.c
@@ -5,10 +5,5 @@
 // arr_len은 배열 arr의 길이입니다.
 double solution(int arr[], size_t arr_len) {
     double answer = 0;
-    for(int i = 0; i < arr_len;i++)
-    {
-        answer += arr[i];
-    }
-    answer /= arr_len;
     return answer;
 }
